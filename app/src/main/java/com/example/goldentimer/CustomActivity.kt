b50723dfe57c4fa49104ce0314cef0f469b1a4c6
sgtlee99 <88578263+sgtlee99@users.noreply.github.com>
2022-11-21 17:41:15 +0900

    numberpicker 구현 -> inflater dialog로 변경중

--- app/src/main/java/com/example/goldentimer/CustomActivity.kt
+++ app/src/main/java/com/example/goldentimer/CustomActivity.kt
@@ -1,16 +1,22 @@
 package com.example.goldentimer
 
 import android.annotation.SuppressLint
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.goldentimer.adapter.Menu_Adapter
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import kotlinx.android.synthetic.main.activity_custom.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.timer
+
 
 class CustomActivity : AppCompatActivity(), View.OnClickListener {
     val TAG: String = "[GT]<CustomActivity>"
@@ -25,6 +31,8 @@ class CustomActivity : AppCompatActivity(), View.OnClickListener {
         //이미지는 기본 에셋제공하여 선택하는 구조
         //버튼 하나를 선택했을때 다른버튼들의 채도가 낮아지며
         //아래의 메뉴 리스트의 아이템들이 변경된다
+        //====================================================
+        var alarm_title : String = timer_title.text.toString()               //알람제목
 
         noodle.setOnClickListener(this)
         fry.setOnClickListener(this)
@@ -34,10 +42,40 @@ class CustomActivity : AppCompatActivity(), View.OnClickListener {
         recyclerview_menu.adapter = adapter
         //면을 선택했을때
         adapter.setOnItemClickListener { item, view ->
+            var selected : String = ""
+            selected = (item as Menu_Adapter).menu                            //선택한 미리보기 이미지
+            Log.d(TAG, selected)     //선택한 메뉴가 뭔지 보여주는 LOG
+            //이미지 리소스 하드 코딩
+            when (selected) {
+                "라면" -> menu_preview_img.setImageResource(R.drawable.ramen)
+                "소면" -> menu_preview_img.setImageResource(R.drawable.soba)
+                "우동" -> menu_preview_img.setImageResource(R.drawable.udon)
+                "파스타" -> menu_preview_img.setImageResource(R.drawable.pasta)
+                else -> menu_preview_img.setImageResource(R.drawable.default_image)
+            }
+        }
+        test_button.setOnClickListener {
 
-            Log.d(TAG, (item as Menu_Adapter).menu)     //선택한 메뉴가 뭔지 보여주는 LOG
+        }
+        //시간을 선택하는 NumberPicker
+//        timer_number_picker_min.maxValue = 60
+//        timer_number_picker_min.minValue = 0
 
+//        val timer_picker_min_text = timer_number_picker_min
+//        val timer_picker_min = timer_number_picker_min.apply {
+//            maxValue = 60
+//            minValue = 0
+//            wrapSelectorWheel = false
+//            setOnValueChangedListener {
+//                    numberPicker, i, i2 ->
+//
+//            }
+//        }
+//        timer_number_picker_sec.maxValue = 59
+//        timer_number_picker_sec.minValue = 0
 
+        timer_save_button.setOnClickListener {
+            ReConfirmDialog()
         }
     }
 
@@ -105,6 +143,24 @@ class CustomActivity : AppCompatActivity(), View.OnClickListener {
         fry.setBackgroundColor(R.color.dark_red)
         bake.setBackgroundColor(R.color.dark_red)
         boil.setBackgroundColor(R.color.bright_red)
+    }
+
+
+
+    fun ReConfirmDialog() {     //설정한 데이터가 맞는지 재확인하는 dialog
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("입력하신 정보가 맞나요?")
+        dialog.setMessage("")
+        dialog.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+            //mainactivity로 이동
+            val intent = Intent(this, MainActivity::class.java)
+            //전 액티비티 지워줌
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            //토스트 메시지
+            Toast.makeText(this, "타이머가 저장되었습니다", Toast.LENGTH_SHORT).show()
+        })
+        dialog.show()
     }
 
 }
